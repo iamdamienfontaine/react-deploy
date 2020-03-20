69bd8b41021e97ebad382fccb507312e0e3d17f1
Damien FONTAINE <279530@supinfo.com>
2020-03-20 10:54:18 +0100

    Updates

---
+++ precache-manifest.1d8c1375efbaa027bfee75af93f019a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6aba21560fa571a18500d0f102d3e26",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "ffc3e36753e9b960b157",
+    "url": "/react-deploy/static/css/main.1a1a7b80.chunk.css"
+  },
+  {
+    "revision": "cbb768cfb30849668c75",
+    "url": "/react-deploy/static/js/2.b9bf5f56.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b9bf5f56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc3e36753e9b960b157",
+    "url": "/react-deploy/static/js/main.45149afa.chunk.js"
+  },
+  {
+    "revision": "4a8bebede5974b130c4b",
+    "url": "/react-deploy/static/js/runtime-main.fb319748.js"
+  },
+  {
+    "revision": "fb2994354210d958a299fb095f73adea",
+    "url": "/react-deploy/static/media/profileImage.fb299435.jpg"
+  }
+]);
